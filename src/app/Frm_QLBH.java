5ff7d38575f776417a0a3298546c19e4c2f17b91
TrongPhan1011 <huutrong2k1@gmail.com>
2021-10-06 12:02:07 +0700

    update frmQLBH

--- src/app/Frm_QLBH.java
+++ src/app/Frm_QLBH.java
@@ -32,6 +32,8 @@ import java.awt.GridLayout;
 import java.awt.FlowLayout;
 import java.awt.CardLayout;
 import java.awt.GridBagLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.JRadioButton;
 
 public class Frm_QLBH extends JPanel implements ActionListener {
 
@@ -41,6 +43,7 @@ public class Frm_QLBH extends JPanel implements ActionListener {
 	private Panel pMain;
 	private JTextField textField;
 	private JTextField txtTim;
+	private JTextField textField_1;
 	
 	public Panel getFrmQLBH() {
 		return this.pMain;
@@ -92,7 +95,7 @@ public class Frm_QLBH extends JPanel implements ActionListener {
 		btnDSHD.setFont(new Font("SansSerif", Font.BOLD, 14));
 		btnDSHD.setBorder(new LineBorder(new Color(0, 146, 182), 2, true));
 		btnDSHD.setBackground(new Color(114, 23, 153));
-		btnDSHD.setBounds(1013, 11, 194, 33);
+		btnDSHD.setBounds(1043, 13, 194, 33);
 		pMain.add(btnDSHD);
 		
 		JLabel lblHeaderPhong = new JLabel("Phòng");
@@ -112,7 +115,7 @@ public class Frm_QLBH extends JPanel implements ActionListener {
 		pPhong.setLayout(new GridLayout(0, 4, 0, 0));
 		
 		
-		scrollPane.setBounds(46, 78, 1191, 108);
+		scrollPane.setBounds(37, 78, 1200, 108);
 		pMain.add(scrollPane);
 		
 		JLabel lblSubPhong = new JLabel("Phòng : ");
@@ -127,32 +130,32 @@ public class Frm_QLBH extends JPanel implements ActionListener {
 		
 		JLabel lblSubTenKH = new JLabel("Tên khách hàng : ");
 		lblSubTenKH.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		lblSubTenKH.setBounds(192, 201, 120, 26);
+		lblSubTenKH.setBounds(158, 201, 295, 26);
 		pMain.add(lblSubTenKH);
 		
 		JLabel lblTenKH = new JLabel("Phan Hữu Trọng");
 		lblTenKH.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 15));
-		lblTenKH.setBounds(310, 201, 194, 26);
+		lblTenKH.setBounds(276, 201, 177, 26);
 		pMain.add(lblTenKH);
 		
 		JLabel lblSubGioVao = new JLabel("Giờ vào: ");
 		lblSubGioVao.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		lblSubGioVao.setBounds(514, 201, 61, 26);
+		lblSubGioVao.setBounds(474, 201, 61, 26);
 		pMain.add(lblSubGioVao);
 		
 		JLabel lblGioVao = new JLabel("15h : 30");
 		lblGioVao.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 15));
-		lblGioVao.setBounds(575, 201, 83, 26);
+		lblGioVao.setBounds(535, 201, 83, 26);
 		pMain.add(lblGioVao);
 		
 		JLabel lblSubGioRa = new JLabel("Giờ ra: ");
 		lblSubGioRa.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		lblSubGioRa.setBounds(675, 201, 61, 26);
+		lblSubGioRa.setBounds(628, 201, 61, 26);
 		pMain.add(lblSubGioRa);
 		
 		JComboBox<String> cbbGioRa = new JComboBox<String>();
 		cbbGioRa.setBackground(Color.WHITE);
-		cbbGioRa.setBounds(731, 205, 47, 22);
+		cbbGioRa.setBounds(684, 205, 47, 22);
 		cbbGioRa.setFont(new Font("SansSerif", Font.PLAIN, 15));
 		
 		for(int i=0 ; i <24;i++ ) {
@@ -162,17 +165,95 @@ public class Frm_QLBH extends JPanel implements ActionListener {
 		
 		JLabel blbSubAfterGioRa = new JLabel(":");
 		blbSubAfterGioRa.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		blbSubAfterGioRa.setBounds(780, 209, 6, 14);
+		blbSubAfterGioRa.setBounds(733, 209, 6, 14);
 		pMain.add(blbSubAfterGioRa);
 		
 		JComboBox<String> cbbPhutRa = new JComboBox<String>();
 		cbbPhutRa.setFont(new Font("SansSerif", Font.PLAIN, 15));
 		cbbPhutRa.setBackground(Color.WHITE);
-		cbbPhutRa.setBounds(787, 205, 47, 22);
+		cbbPhutRa.setBounds(740, 205, 47, 22);
 		for(int i =0; i<60; i++) {
 			cbbPhutRa.addItem(""+i);
 		}
 		pMain.add(cbbPhutRa);
+		
+		JLabel lblSubPhuThu = new JLabel("Phụ thu: ");
+		lblSubPhuThu.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		lblSubPhuThu.setBounds(870, 201, 61, 26);
+		pMain.add(lblSubPhuThu);
+		
+		JComboBox<String> cbbPhuThu = new JComboBox<String>();
+		cbbPhuThu.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		cbbPhuThu.setBackground(Color.WHITE);
+		cbbPhuThu.setBounds(934, 205, 90, 22);
+//		
+//		int namHienTai = dNgayHienTai.getYear()+10;
+//		int namBatDau = namHienTai -20;
+//		for(int i =namBatDau; i <= namHienTai; i++) {
+//			cbbPhuThu.addItem(""+i);
+//		}
+//		cbbPhuThu.setSelectedItem(dNgayHienTai.getYear()+"");
+//		
+		String sPhuThu [] = {"Không","Ngày lễ","Cuối tuần"};
+		for(int i =0; i< sPhuThu.length;i++) {
+			cbbPhuThu.addItem(sPhuThu[i]);
+		}
+		pMain.add(cbbPhuThu);
+		
+		JLabel lblNgayLap = new JLabel("10/11/2021");
+		lblNgayLap.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC, 15));
+		lblNgayLap.setBounds(1154, 201, 83, 26);
+		pMain.add(lblNgayLap);
+		
+		JLabel lblSubNgayLap = new JLabel("Ngày lập:");
+		lblSubNgayLap.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		lblSubNgayLap.setBounds(1084, 201, 71, 26);
+		pMain.add(lblSubNgayLap);
+		
+		JPanel pDichVu = new JPanel();
+		pDichVu.setBorder(new TitledBorder(new LineBorder(new Color(114, 23 ,153), 1, true), "Dịch vụ ", TitledBorder.CENTER, TitledBorder.TOP, null, Color.BLACK));
+		pDichVu.setBackground(Color.WHITE);
+		pDichVu.setBounds(37, 235, 255, 335);
+		pMain.add(pDichVu);
+		pDichVu.setLayout(null);
+		
+		JLabel lblSubLMH = new JLabel("Loại mặt hàng: ");
+		lblSubLMH.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		lblSubLMH.setBounds(10, 38, 102, 26);
+		pDichVu.add(lblSubLMH);
+		
+		JComboBox<String> cbbLoaiMH = new JComboBox<String>();
+		cbbLoaiMH.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		cbbLoaiMH.setBackground(Color.WHITE);
+		cbbLoaiMH.setBounds(112, 36, 133, 30);
+		pDichVu.add(cbbLoaiMH);
+		
+		JLabel lblSubTenMH = new JLabel("Tên mặt hàng: ");
+		lblSubTenMH.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		lblSubTenMH.setBounds(10, 90, 102, 26);
+		pDichVu.add(lblSubTenMH);
+		
+		JComboBox<String> cbbTenMH = new JComboBox<String>();
+		cbbTenMH.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		cbbTenMH.setBackground(Color.WHITE);
+		cbbTenMH.setBounds(112, 88, 133, 30);
+		pDichVu.add(cbbTenMH);
+		
+		JLabel lblSoluongMH = new JLabel("Số lượng:");
+		lblSoluongMH.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		lblSoluongMH.setBounds(10, 142, 84, 26);
+		pDichVu.add(lblSoluongMH);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(112, 141, 133, 30);
+		pDichVu.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JRadioButton rdbtnGiamSL = new JRadioButton("Giảm số lượng");
+		rdbtnGiamSL.setBackground(Color.WHITE);
+		rdbtnGiamSL.setFont(new Font("SansSerif", Font.PLAIN, 15));
+		rdbtnGiamSL.setBounds(62, 185, 147, 35);
+		pDichVu.add(rdbtnGiamSL);
 		
 		
 
